094bcf745326e050add510e3fc74b6ebd81310da
Kaustubh Zagade <38424988+kaustubhz@users.noreply.github.com>
2019-10-08 16:35:14 +0530

    C Program to create  processes
    5 processes of same parent are being created

--- Day4/que1.c
+++ Day4/que1.c
@@ -3,31 +3,39 @@
 #include<unistd.h>
 int main()
 {
-	pid_t pid;
-
-	pid=fork();
-
-	//For a child process, pid is 0
-	if(pid==0)
+	int i;
+	pid_t pid[5];
+	printf("\nCreating a 5 Processes\n");
+	for(i=1;i<=5;i++)
 	{
-		/*sleep(5);
-		printf("\n Child process created");
-		printf("\nChild process ID: %d",getpid());
-		printf("\nChild parent process ID: %d",getppid());
-		sleep(15);
-//		while(1);*/
-		exit(0);		
-	}
-	else
-	{
-		sleep(10);
-		printf("\nParent process created");
-		printf("\nParent Process ID: %d",getpid());
-		printf("\nChild's process ID: %d",pid);
-//		sleep(40);
-		sleep(50);
-	}
-	printf("\nBoth processes at the end");
-	
+		pid[i]=fork();
+
+
+		//For a child process, pid is 0
+		if(pid[i]==0)
+		{
+			sleep(5);
+			printf("\n Child process created");
+			printf("\n Child process ID: %d",getpid());
+			printf("\n Child parent process ID: %d",getppid());
+			exit(0);
+	//		sleep(15);
+			//		while(1);
+			//		exit(0);		
+		}
+		
+	}	
+	/*for( )	
+	//		sleep(10);
+			printf("\nParent process created");
+			printf("\nParent Process ID: %d",getpid());
+			printf("\nChild's process ID: %d",pid);
+			//		sleep(40);
+	//		sleep(50);
+			int status;
+			waitpid(pid,&status,NULL);
+			printf("\n Status of exited process: %d",status);
+		printf("\nBoth processes at the end");
+	*/	
 	return 0;
 }
